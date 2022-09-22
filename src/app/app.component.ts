683f1dd3f1d3e7b560d2a4a6d270cf8ade13ff08
VERNIERELoic <loic.verniere@cpe.fr>
2022-09-22 12:28:07 +0200

    locomotive ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import LocomotiveScroll from 'locomotive-scroll';
+import { ResizeObserver } from '@juggle/resize-observer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isCollapsed = false;
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('scrollContent')
+  scrollContent!: ElementRef;
+
+  scroll;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+      getDirection: true
+    });
+  }
+
+  ngAfterViewInit() {
+    const ro = new ResizeObserver((entries, observer) => {
+      entries.forEach((entry, index) => {
+        const { inlineSize: width, blockSize: height } = entry.contentBoxSize[0];
+        if (this.scroll) {
+          this.scroll.update();
+        }
+      });
+    });
+
+    ro.observe(this.scrollContent.nativeElement);
+  }
+
 }
