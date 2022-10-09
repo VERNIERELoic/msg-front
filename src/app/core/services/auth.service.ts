4b6a143e74b0558687b02046adf4ed9023c4bd63
VERNIERELoic <loic.verniere@cpe.fr>
2022-10-09 21:18:19 +0200

    feat : connection ok

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-
+import { shareReplay } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,26 @@ export class AuthService {
 
   addUser(user: any): any {
     return this.http.post(`${environment.apiUrl}/register`, user);
+  }
+
+  login(user: any): any {
+    return this.http.post(`${environment.apiUrl}/login`, user).pipe(shareReplay());
+  }
+
+  logout() {
+    this.destroyToken();
+    this.router.navigate(['/login'])
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem("token");
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem("token", token);
+  }
+
+  destroyToken() {
+    localStorage.removeItem("token");
   }
 }
