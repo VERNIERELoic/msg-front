6d0edd915113389757c9830284478577b2e459c9
VERNIERELoic <loic.verniere@cpe.fr>
2022-10-17 16:22:36 +0200

    auth finish

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -27,7 +27,6 @@ export class LoginComponent implements OnInit {
       });
   }
 
-
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       email: [null, [Validators.required]],
--- src/app/pages/user/user.component.spec.ts
+++ src/app/pages/user/user.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ExploreComponent } from './explore.component';
+import { UserComponent } from './user.component';
 
-describe('ExploreComponent', () => {
-  let component: ExploreComponent;
-  let fixture: ComponentFixture<ExploreComponent>;
+describe('UserComponent', () => {
+  let component: UserComponent;
+  let fixture: ComponentFixture<UserComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExploreComponent ]
+      declarations: [ UserComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ExploreComponent);
+    fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
