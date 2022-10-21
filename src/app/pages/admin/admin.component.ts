21475a65968bf519a637b2097600e437f1e79d3a
VERNIERELoic <loic.verniere@cpe.fr>
2022-10-22 00:43:50 +0200

    feat: console admin

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { NzCalendarMode } from 'ng-zorro-antd/calendar';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  date = new Date();
+  mode: NzCalendarMode = 'month';
+  isVisible = false;
+  isAdmin = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  panelChange(change: { date: Date; mode: string }): void {
+    console.log(change.date, change.mode);
+  }
+
+  select(ret: Date): Date {
+    console.log('nzSelectChange', ret);
+    return ret;
+  }
+
+  checkpassdate = (date: Date) => {
+    console.log(date, date > new Date)
+    return date < new Date();
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.'
+  ];
 
 }
